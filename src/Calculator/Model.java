fb46386913c191af0990e941b8ffe08313823e35
Gavin Morris <themixedmaster@gmail.com>
2017-03-18 14:31:59 -0700

    week49
    koala

--- src/Calculator/Model.java
+++ src/Calculator/Model.java
@@ -1,13 +1,53 @@
 package Calculator;
 
 public class Model {
-	String inputOne;
-	String inputTwo;
-	String inputTree;
-	String output;
-	public String addNum(String num){
-		inputOne = inputOne + num;
-		return inputOne;
+	String inputOne = "";
+	String inputTwo = "";
+	String inputTree = "";
+	String output = "";
+	boolean operatorPressed = false;
+	public void addNum(String num){
+		if(num.equals("=")){
+			calculate();
+		}else if(num.equals("+")||num.equals("+")||num.equals("X")||num.equals("÷")||num.equals("%")){
+			inputTwo = num;
+			operatorPressed = true;
+		}else if(operatorPressed == true){
+			inputTree = inputTree + num;
+		}else if(operatorPressed == false){
+			inputOne = inputOne + num;
+		}
+		if(num.equals("Clear")){
+			operatorPressed = false;
+			inputOne = "";
+			inputTwo = "";
+			inputTree = "";
+			output = "";
+		}
+	}
+	private void calculate() {
+		int num1 = Integer.parseInt(inputOne);
+		int num3 = Integer.parseInt(inputTree);
+		if(inputTwo.equals("+")){
+			int answer = num1 + num3;
+			output = "" + answer;
+		}
+		if(inputTwo.equals("-")){
+			int answer = num1 - num3;
+			output = "" + answer;
+		}
+		if(inputTwo.equals("X")){
+			int answer = num1 * num3;
+			output = "" + answer;
+		}
+		if(inputTwo.equals("÷")){
+			int answer = num1 / num3;
+			output = "" + answer;
+		}
+		if(inputTwo.equals("%")){
+			int answer = num1 % num3;
+			output = "" + answer;
+		}
 	}
 	public String getInputOne() {
 		return inputOne;
@@ -33,4 +73,5 @@ public class Model {
 	public void setOutput(String output) {
 		this.output = output;
 	}
+	
 }
